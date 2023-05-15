2f757cf69c4f0e2741a02ff881ac1a61ad727f17
OrangeCash090 <97858392+OrangeCash090@users.noreply.github.com>
2023-05-14 20:21:34 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3001
+const path = require('path');
+
+app.use(bodyParser.json())
+
+var PlayerPosition = null
+var Players = {}
+
+// Render Html File
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/getdata', function(req, res) {
+  res.send({PlayerPosition: PlayerPosition, Players: Players})
+});
+
+app.post('/senddata', function (req, res) {
+  PlayerPosition = req.body.PlayerPosition
+  Players = req.body.Players
+});
+
+app.listen(port, () => {
+  // do nothing
+})
